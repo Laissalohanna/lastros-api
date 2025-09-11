63e55e237e7305ee1bc1b4c307a00f8760d59525
Laissalohanna <laissalohanna360@gmail.com>
2025-09-11 16:42:54 -0300

    feat: status code

--- src/app.py
+++ src/app.py
@@ -1,5 +1,5 @@
 import falcon
-from resources import HealthResource
+from resources import HealthResource, BallastsResource
 from middlewares import (LoggingMiddleware, ErrorHandlerMiddleware, AuthMiddleware, CORSMiddleware)
 
 app = falcon.App(
@@ -12,3 +12,6 @@ app = falcon.App(
 )
 
 app.add_route("/health", HealthResource())
+
+ballasts_resource = BallastsResource()
+app.add_route("/ballasts/project/{project_uid}/file/{file_name}", ballasts_resource, suffix="by_file_name_and_project_uid")
--- src/errors/custom_error.py
+++ src/errors/custom_error.py
@@ -1,21 +1,31 @@
 class CustomError(Exception):
     """Base class for custom exceptions."""
+    status_code = 500 
+
     def __init__(self, message: str, translate: str = None):
         super().__init__(message)
         self.message = message
         self.translate = translate or message
 
-    def __str__(self):
-        return self.message
+    def to_dict(self):
+        return {
+            "error": self.message,
+            "translate": self.translate
+        }
 
 
 class NotFoundProjectUID(CustomError):
+    status_code = 404 
+
     def __init__(self, project_uid: str):
         message = f"The project Uid with key: {project_uid}, was not found."
         translate = f"O projeto com a chave: {project_uid}, não foi encontrado."
         super().__init__(message, translate)
 
+
 class NotFoundFileName(CustomError):
+    status_code = 404 
+
     def __init__(self, file_name: str, project_uid: str):
         message = f"Could not find file with name: {file_name}, for project: {project_uid}."
         translate = f"Não foi possível encontrar o arquivo com o nome: {file_name}, para o projeto: {project_uid}."
